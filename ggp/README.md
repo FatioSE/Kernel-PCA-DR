9d63481675a3fd794a080cdebb4fe9434a9a24fc
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-06-24 13:00:43 +0000

    modified: README.md

--- ggp/README.md
+++ ggp/README.md
@@ -21,4 +21,4 @@ To execute tests run the command in the ggp folder:
 The following libraries were used in this project:
 
 * [Clikt](https://github.com/ajalt/clikt): make it possible to create command line applications using Kotlin 
-* [GSON
+* [GSON](https://github.com/google/gson): parser objects from/to JSON
---
+++ ggp/build.gradle.kts
@@ -0,0 +1,34 @@
+
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "io.wcode"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Clikt
+    implementation("com.github.ajalt:clikt:2.3.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    // Kafka
+    implementation ("org.apache.kafka:kafka-clients:2.4.0")
+
+    // Testing dependencies
+    testImplementation("org.assertj:assertj-core:3.12.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+}
+
+tasks {
