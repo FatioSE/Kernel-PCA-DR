2fbd7dc2be6db37b6ec1b91e68caed05f574e8f2
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-09-18 11:35:54 +0000

    Miscellaneous non-functional changes (spelling, etc).

--- ggp/src/main/kotlin/kafka/KafkaController.kt
+++ ggp/src/main/kotlin/kafka/KafkaController.kt
@@ -5,4 +5,7 @@ import models.StreamIndividual
 
 
 object KafkaController {
-    val kafka = Kafka("kafka-server:9
+    val kafka = Kafka("kafka-server:9092")
+    val processorTopic = "sk-individual-topic"
+
+    fun sendIndividual(streamIndividual: StreamIndividua
