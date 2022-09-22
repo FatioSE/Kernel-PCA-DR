337c298fc9ecf534b02267569d4fde845c0eaf7e
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-09-22 16:09:39 +0000

    beta2

--- ggp/src/main/kotlin/kafka/Producer.kt
+++ ggp/src/main/kotlin/kafka/Producer.kt
@@ -2,4 +2,6 @@ package kafka
 
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
-import o
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+i
