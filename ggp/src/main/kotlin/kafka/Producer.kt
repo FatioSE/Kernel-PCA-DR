0448b2ffbf38ee62c4ec7f7c4dc7a796fda08065
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-09-24 22:48:50 +0000

    added initial code

--- ggp/src/main/kotlin/kafka/Producer.kt
+++ ggp/src/main/kotlin/kafka/Producer.kt
@@ -4,4 +4,8 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
-i
+import java.util.*
+
+class Producer(kafka: Kafka, private val topic: String) {
+
+    private val 
