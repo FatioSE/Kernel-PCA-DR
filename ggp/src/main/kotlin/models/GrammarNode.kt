684f052c72e11dd95565700f19a3445ddc42cfcf
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-10-02 06:20:10 +0000

    clean up warnings

--- ggp/src/main/kotlin/models/GrammarNode.kt
+++ ggp/src/main/kotlin/models/GrammarNode.kt
@@ -3,4 +3,7 @@ package models
 data class GrammarNode(
     val name: String,
     val key: String,
-    val ter
+    val terminal: Boolean = true,
+    val probability: Double = 1.0,
+    val children: List<GrammarNode> = emptyList()
+)
