47da2607c83a8df70117709eedd1c4cb27bd92b4
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-10-05 21:05:43 +0000

    Internal cleanup.

--- ggp/src/main/kotlin/models/Individual.kt
+++ ggp/src/main/kotlin/models/Individual.kt
@@ -1,2 +1,12 @@
 
 package models
+
+import java.util.*
+
+data class Individual(
+    val id: String = UUID.randomUUID().toString(),
+    var tree: IndividualTreeNode? = null,
+    var fitness: Double = -1.0,
+    var componentsMap: Map<String, IndividualTreeNode>
+) {
+}
---
+++ ggp/src/main/kotlin/models/IndividualTreeNode.kt
@@ -0,0 +1,6 @@
+package models
+
+data class IndividualTreeNode(
+    val key: String,
+    val terminal: Boolean,
+    val co
