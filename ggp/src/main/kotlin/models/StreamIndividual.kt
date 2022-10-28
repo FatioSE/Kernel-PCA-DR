3009e11588a61065ffaeeb25bf36bc65783b436b
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-10-28 20:43:43 +0000

    update node

--- ggp/src/main/kotlin/models/StreamIndividual.kt
+++ ggp/src/main/kotlin/models/StreamIndividual.kt
@@ -1,3 +1,8 @@
 package models
 
-import com.google.gson.annota
+import com.google.gson.annotations.SerializedName
+
+data class StreamIndividual(
+    var id: String,
+    @SerializedName("import_str")
+    var 
