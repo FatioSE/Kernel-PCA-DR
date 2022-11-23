825ecb41beb6211d5edd0eec7b6c7fcd472cb93e
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-11-23 20:50:29 +0000

    Update webz-war.md

--- ggp/src/main/kotlin/utils/FileUtils.kt
+++ ggp/src/main/kotlin/utils/FileUtils.kt
@@ -4,4 +4,7 @@ import java.io.File
 
 object FileUtils {
 
-    fun readFile(fileName: String): St
+    fun readFile(fileName: String): String? {
+        val file = File(fileName)
+        return if (file.exists()) {
+            file.readText(Charsets.UT
