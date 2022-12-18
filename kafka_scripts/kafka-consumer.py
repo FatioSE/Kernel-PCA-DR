bb84007c9d3171fafd5e7c1cb3176b0750dacbfc
FatioSE <93031906+FatioSE@users.noreply.github.com>
2022-12-18 15:15:47 +0000

    Update: readme

--- kafka_scripts/kafka-consumer.py
+++ kafka_scripts/kafka-consumer.py
@@ -2,4 +2,8 @@ from kafka import KafkaConsumer
 
 # To consume latest messages and auto-commit offsets
 consumer = KafkaConsumer('kafka-python-topic',
-                        api
+                        api_version=(0,11,5),
+                        bootstrap_servers=['kafka-server:9092'])
+
+for message in consumer:
+    # me
