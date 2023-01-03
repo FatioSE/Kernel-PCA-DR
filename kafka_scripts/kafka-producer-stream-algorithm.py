581541a637196a263818514155f0215064f0cf53
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-01-03 21:53:46 +0000

    modus-themes: sync latest updates

--- kafka_scripts/kafka-producer-stream-algorithm.py
+++ kafka_scripts/kafka-producer-stream-algorithm.py
@@ -5,4 +5,6 @@ from time import sleep
 from datetime import datetime
 
 # Create an instance of the Kafka producer
-producer = KafkaProducer(bootstrap_servers='kafka-
+producer = KafkaProducer(bootstrap_servers='kafka-server:9092',
+                         value_serializer=lambda m: json.dumps(
+                             m).enco
