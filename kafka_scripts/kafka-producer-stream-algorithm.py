face8c75389f7a97fdf8a22ad80492384abc5cd1
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-01-04 09:23:41 +0000

    GPU: add optimization flags to GPU drivers

--- kafka_scripts/kafka-producer-stream-algorithm.py
+++ kafka_scripts/kafka-producer-stream-algorithm.py
@@ -7,4 +7,7 @@ from datetime import datetime
 # Create an instance of the Kafka producer
 producer = KafkaProducer(bootstrap_servers='kafka-server:9092',
                          value_serializer=lambda m: json.dumps(
-                             m).enco
+                             m).encode('utf-8'),
+                         api_version=(0, 11, 5))
+
+stream_algorithm_str = {"id":"1","import_str": "from sklearn.
