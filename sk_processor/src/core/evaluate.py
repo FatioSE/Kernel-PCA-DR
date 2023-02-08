b1d2cb310b214e635ffe8ee7928ed41e78410f8f
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-02-08 09:59:43 +0000

    fix mock

--- sk_processor/src/core/evaluate.py
+++ sk_processor/src/core/evaluate.py
@@ -1,3 +1,6 @@
 from multiprocessing import Process, Queue
 from queue import Empty
-from models.evaluation_result impor
+from models.evaluation_result import EvaluationResult
+from models.stream_individual import StreamIndividual
+
+from core.evaluate_indiv
