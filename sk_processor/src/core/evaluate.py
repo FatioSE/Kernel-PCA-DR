f51516a926b0c7feb1fd520206f2045e7051930b
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-02-17 14:38:34 +0000

    More formatting tweaks

--- sk_processor/src/core/evaluate.py
+++ sk_processor/src/core/evaluate.py
@@ -6,4 +6,6 @@ from models.stream_individual import StreamIndividual
 from core.evaluate_individual import evaluate_individual
 
 def evaluate(stream_individual: StreamIndividual) -> EvaluationResult:
-  
+    evaluate_result = EvaluationResult(individual_id = stream_individual.id)
+
+    process_queue = Queue(
