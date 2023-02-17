bf09fcc240bb777a831b871edf2fe5a5de34d7c9
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-02-17 09:37:15 +0000

    vhs

--- sk_processor/src/core/evaluate.py
+++ sk_processor/src/core/evaluate.py
@@ -3,4 +3,7 @@ from queue import Empty
 from models.evaluation_result import EvaluationResult
 from models.stream_individual import StreamIndividual
 
-from core.evaluate_indiv
+from core.evaluate_individual import evaluate_individual
+
+def evaluate(stream_individual: StreamIndividual) -> EvaluationResult:
+  
