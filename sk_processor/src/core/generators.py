36299923b09aaad52b780221c542c940a3e9e862
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-01 18:44:20 +0000

    update debug

---
+++ sk_processor/src/core/generators.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from models.algorithm import Algorithm
+
+def generate_model(algorithm : Algorithm):
+    exec(algorithm.import_str)
+    generated_model = eval(algorithm.get_model_str())
+    return generated_model
