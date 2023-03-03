19bb0dd1c4c66479cd1e4e63f9fdc191b3d2da66
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-03 13:41:37 +0000

    update 6

--- sk_processor/src/models/algorithm.py
+++ sk_processor/src/models/algorithm.py
@@ -2,3 +2,8 @@ class Algorithm:
     
     def __init__(self,import_str: str,alg_str: str,parameters_str: list):
         self.import_str = import_str
+        self.alg_str = alg_str
+        self.parameters_str = parameters_str
+
+    def get_model_str(self):
+ 
