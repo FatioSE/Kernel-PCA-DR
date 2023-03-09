d8dafdfdc08de3b36495a6850a6923d7d4229b48
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-09 08:05:45 +0000

    Update gomme

--- sk_processor/src/models/evaluation_result.py
+++ sk_processor/src/models/evaluation_result.py
@@ -1,3 +1,4 @@
 class EvaluationResult:
 
-    def __init__(self, individu
+    def __init__(self, individual_id: str, accuracy_test: float = -1.0, f1_test: float = -1.0):
+        self.individual_id = individual_id
