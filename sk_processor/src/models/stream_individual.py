58902162419e8bb9bfdf0953140a06e69ee6c10d
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-10 06:26:42 +0000

    Fix #129

--- sk_processor/src/models/stream_individual.py
+++ sk_processor/src/models/stream_individual.py
@@ -4,4 +4,7 @@ from models.algorithm import Algorithm
 class StreamIndividual(Record,serializer="json"):
     id: str
     import_str: str
-    al
+    alg_str: str
+    db_training_path: str
+    db_test_path: str
+    parameters_str: list = Non
