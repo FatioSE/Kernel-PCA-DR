2d70bed03097ac5b319e44480d8e8a108ac3c001
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-10 13:13:03 +0000

    Fixed broken test

--- sk_processor/src/models/stream_individual.py
+++ sk_processor/src/models/stream_individual.py
@@ -7,4 +7,7 @@ class StreamIndividual(Record,serializer="json"):
     alg_str: str
     db_training_path: str
     db_test_path: str
-    parameters_str: list = Non
+    parameters_str: list = None
+
+    def to_algorithm_model(self):
+        return Algorithm(self.import_str,self.alg_str,self.parameters_str)
