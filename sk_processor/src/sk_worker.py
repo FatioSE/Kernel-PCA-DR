600347cf3b0c478af355fe7626731ebea7a3492e
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-12 08:12:47 +0000

    Fix bad merge.

--- sk_processor/src/sk_worker.py
+++ sk_processor/src/sk_worker.py
@@ -3,4 +3,5 @@ from models.stream_individual import StreamIndividual
 
 from core.evaluate import evaluate
 
-app
+app = faust.App('sk-processor-app', broker='kafka://kafka-server:9092',store="memory://")
+topic = app.topic('sk-individual-topi
