a1156cc5707b8804a25fc5410e80d4449c61dec6
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-12 11:30:56 +0000

    Javadoc improvements.

--- sk_processor/src/sk_worker.py
+++ sk_processor/src/sk_worker.py
@@ -4,4 +4,7 @@ from models.stream_individual import StreamIndividual
 from core.evaluate import evaluate
 
 app = faust.App('sk-processor-app', broker='kafka://kafka-server:9092',store="memory://")
-topic = app.topic('sk-individual-topi
+topic = app.topic('sk-individual-topic', value_type=StreamIndividual)
+
+@app.agent(topic)
+async def process_individual(individuals: faust.Stre
