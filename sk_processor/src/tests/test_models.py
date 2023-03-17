d789ba8b3d0edbb6d641de62906d982b02e666d1
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-17 07:44:08 +0000

    remove binary file

--- sk_processor/src/tests/test_models.py
+++ sk_processor/src/tests/test_models.py
@@ -3,4 +3,6 @@ from models.algorithm import Algorithm
 from models.stream_individual import StreamIndividual
 from models.evaluation_result import EvaluationResult
 
-def tes
+def test_algorithm_model():
+    algorithm = Algorithm(
+        "from sklearn.tree import DecisionTreeClassifier"
