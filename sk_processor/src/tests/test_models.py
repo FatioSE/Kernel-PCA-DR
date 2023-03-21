bf5a2d136ce9a2087d8cd7b77abf2b341b9500b3
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-21 07:04:28 +0000

    4th

--- sk_processor/src/tests/test_models.py
+++ sk_processor/src/tests/test_models.py
@@ -6,4 +6,5 @@ from models.evaluation_result import EvaluationResult
 def test_algorithm_model():
     algorithm = Algorithm(
         "from sklearn.tree import DecisionTreeClassifier", "DecisionTreeClassifier", [])
-    assert algorithm.import_str == "from sklearn.tree
+    assert algorithm.import_str == "from sklearn.tree import DecisionTreeClassifier", "test failed"
+    assert algorithm.alg_str == "DecisionTreeClassifier",
