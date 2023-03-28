768d9529527f96e9be2ba18b0abe5baefcde1949
FatioSE <93031906+FatioSE@users.noreply.github.com>
2023-03-28 13:00:28 +0000

    v0.13-preview.120.27+112

--- sk_processor/src/tests/test_models.py
+++ sk_processor/src/tests/test_models.py
@@ -19,4 +19,5 @@ def test_algorithm_from_stream_algorithm():
                                         db_test_path="test.csv")
     algorithm = stream_algorithm.to_algorithm_model()
     assert algorithm.import_str == "from sklearn.tree import DecisionTreeClassifier", "test failed"
-    assert algorithm.alg_str =
+    assert algorithm.alg_str == "DecisionTreeClassifier",  "test failed"
+    assert stream_algorithm.db_training_path == "trai
